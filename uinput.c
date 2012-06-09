f9f2f8b0dbd6c32b65acbd552f206a7910c23b17
Thomas Bastiani <thom@codehawks.eu>
2012-06-09 13:31:56 +0100

    Removing already commented code

--- uinput.c
+++ uinput.c
@@ -9,51 +9,10 @@
 
 #include "joyput.h"
 
-/*
-int get_next_keyboard_index()
-{
-	DIR *input_dir;
-	struct dirent *entry;
-	int index = 0;
-
-	input_dir = opendir ("/dev/input");
-	if (input_dir == NULL)
-		die("Couldn't open the directory");
-
-	while (entry = readdir (input_dir))
-	{
-		int event_value;
-		if (strncmp(entry->d_name, "event", 5))
-		{
-			continue;
-		}
-
-		event_value = atoi(entry->d_name + 5);
-		if (event_value >= index + 1)
-		{
-			index = event_value + 1;
-		}
-	}
-
-	(void) closedir (input_dir);
-	return index;
-}
-*/
-
 void open_keyboard_device(joydata_t *joydata)
 {
 	int fd;
     struct uinput_user_dev uinput_device = {0};
-	/*
-	int device_index;
-	char device_name[20];
-
-	device_index = get_next_keyboard_index();
-	snprintf(device_name, 20, "/dev/input/event%d", device_index);
-#ifdef DEBUG
-	printf("Creating fake input device at: %s\n", device_name);
-#endif
-	*/
 	
     fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
     if(fd < 0)
