577b9b1f6c7e43679e82e1b258d525c9f481543c
Thomas Bastiani <thom@codehawks.eu>
2012-06-13 22:55:47 +0100

    replacing read_config() stub with (basic) functional code

--- src/translate.c
+++ src/translate.c
@@ -4,25 +4,74 @@
 #include <linux/joystick.h>
 #include <linux/input.h>
 #include <sys/time.h>
+#define __USE_POSIX
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "joyput.h"
 #include "translate.h"
+#include "utils.h"
 
 static button_map *button_map_list;
 
 void read_config()
 {
-	/* This is only a fake config */
-	button_map *button_map_node;
-	button_map_node = (button_map *) malloc(sizeof(button_map));
+	button_map *current_button_map;
+	button_map *button_map_list = NULL;
+	int fd = open("/etc/joyput.conf", O_RDONLY);
+	if (fd == -1)
+		die("Could not open config file /etc/joyput.c.");
 
-	button_map_node->is_an_axis = 0;
-	button_map_node->joy_index = 5;
-	button_map_node->axis_threshold = 0;
-	button_map_node->key_index = KEY_A;
-	button_map_node->next = NULL;
+	/* FIXME Not supporting any conf files bigger than 10000 bytes. */
+	char buffer[10000];
+	int size = read(fd, buffer, 10000);
+	if (size < 0)
+		die("Config file at /etc/joyput.c is empty.");
 
-	set_button_map(button_map_node);
+	close(fd);
+
+	char *line;
+	char *save_line;
+	line = strtok_r(buffer, "\n", &save_line);
+
+	while (line != NULL)
+	{
+		char *save_field; 
+		char *field;
+		int is_an_axis;
+		int joy_index;
+		int key_index;
+
+		field = strtok_r(line, ":", &save_field);
+		is_an_axis = atoi(field);
+
+		field = strtok_r(NULL, ":", &save_field);
+		joy_index = atoi(field);
+		field = strtok_r(NULL, ":", &save_field);
+		key_index = atoi(field);
+
+		if (button_map_list == NULL)
+		{
+			current_button_map = (button_map *) calloc(1, sizeof(button_map));
+			button_map_list = current_button_map;
+		}
+		else
+		{
+			current_button_map->next = (button_map *) calloc(1, sizeof(button_map));
+			current_button_map = current_button_map->next;
+		}
+
+		current_button_map->is_an_axis = is_an_axis;
+		current_button_map->joy_index = joy_index;
+		current_button_map->key_index = key_index;
+
+		/* Next line */
+		line = strtok_r(NULL, "\n", &save_line);
+		/* FIXME How is strtok_r() context destroyed for sure? */
+	}
+
+	set_button_map(button_map_list);
 }
 
 static button_map *get_button_map(void)
